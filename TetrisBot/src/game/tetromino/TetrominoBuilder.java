5a66955b0fb110faaf559633544dfcc89ef99bae
Joosep Jääger <joosep.jaager@gmail.com>
2016-11-19 11:34:00 +0200

    Addded placeholder coordinates to Tetromino constructors. Added some comments.

--- TetrisBot/src/game/tetromino/TetrominoBuilder.java
+++ TetrisBot/src/game/tetromino/TetrominoBuilder.java
@@ -19,38 +19,59 @@ public class TetrominoBuilder {
         switch (shape) {
             case T_4x1:
                 return new Tetromino(new int[][]{
-                        {1, 1, 1, 1}
-                });
+                        {1, 1, 1, 1},				// The shape of a spawned block, make sure it is spawned with the right rotation
+                },
+                0,				// The coordinates of a spawned block
+                0				// Use trial-and error to find the coordinates that match game spawns
+                );
             case T_2x2:
                 return new Tetromino(new int[][]{
                         {1, 1},
                         {1, 1}
-                });
+                },
+                0,
+                0
+                );
             case T_S:
                 return new Tetromino(new int[][]{
                         {0, 1, 1},
                         {1, 1, 0}
-                });
+                },
+                0,
+                0
+                );
             case T_Z:
                 return new Tetromino(new int[][]{
                         {1, 1, 0},
                         {0, 1, 1}
-                });
+                },
+                0,
+                0
+                );
             case T_L:
                 return new Tetromino(new int[][]{
                         {0, 0, 1},
                         {1, 1, 1}
-                });
+                },
+                0,
+                0
+                );
             case T_J:
                 return new Tetromino(new int[][]{
                         {1, 0, 0},
                         {1, 1, 1}
-                });
+                },
+                0,
+                0
+                );
             case T_T:
                 return new Tetromino(new int[][]{
                         {1, 1, 1},
                         {0, 1, 0}
-                });
+                },
+                0,
+                0
+                );
         }
         return null;
     }
